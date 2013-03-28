dffa30549852b397517864b3b59488de8c7d750a
roys <roys3d@gmail.com>
2013-03-27 21:31:56 -0400

    filtre pour <img ... > utilisation de jsonreader

--- UdemCalendrier/src/com/seboid/udemcalendrier/ActivityDebugEvents.java
+++ UdemCalendrier/src/com/seboid/udemcalendrier/ActivityDebugEvents.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
@@ -61,20 +63,86 @@ public class ActivityDebugEvents extends Activity implements OnItemClickListener
 	// web asynchrone
 	//
 
-	class DownloadEventsTask extends AsyncTask<Void,Void,EventsAPI> {
+	class DownloadEventsTask extends AsyncTask<Void,Void,EventsReaderAPI> {
 		@Override
 		protected void onPreExecute() {
 			setProgressBarIndeterminateVisibility(true);
 		}
 
 		@Override
-		protected EventsAPI doInBackground(Void... arg0) {
-			EventsAPI events=new EventsAPI();
-			return events;
+		protected EventsReaderAPI doInBackground(Void... arg0) {
+			//EventsAPI events=new EventsAPI("evenements",null,"2013-03-27","2013-03-27");
+			//EventsAPI events=new EventsAPI("categorie","2","2013-03-27","2013-03-30");
+			//EventsAPI events=new EventsAPI("groupe","7","2013-03-27","2013-03-30");
+			//EventsAPI events=new EventsAPI("souscategorie","7","2013-03-27","2013-03-30");
+			//EventsAPI events=new EventsAPI("serie","cfd4b81e5657376b6",null,null);
+
+			// aujourd'hui a dans une semaine
+			Time now = new Time(Time.getCurrentTimezone());
+			now.setToNow();
+			//now.allDay=true;
+			//now.normalize(true);
+			Log.d("events","today is "+now.format("%y-%m-%d"));
+			now.set(now.toMillis(true)+7*24*3600*1000);
+			Log.d("events","next week is "+now.format("%y-%m-%d"));
+
+			
+//			textViewDay.setText(today.monthDay);             // Day of the month (0-31)
+//			textViewMonth.setText(today.month);              // Month (0-11)
+//			textViewYear.setText(today.year);                // Year 
+//			textViewTime.setText(today.format("%k:%M:%S"));  // Current time
+			
+			EventsReaderAPI e;
+			long d=System.currentTimeMillis();
+			// le 2013-03-27 -> contient un super gros description <img base64 >
+			e=new EventsReaderAPI("evenements",null,"2013-03-27","2013-04-02");
+			d-=System.currentTimeMillis();
+			Log.d("event","week load is "+d/1000.0+" sec");
+
+//			long total=0;
+//			
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-03-27","2013-03-27");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//			
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-03-28","2013-03-28");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-03-29","2013-03-29");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-03-30","2013-03-30");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-03-31","2013-03-31");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-04-01","2013-04-01");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//
+//			d=System.currentTimeMillis();
+//			e=new EventsAPI("evenements",null,"2013-04-02","2013-04-02");
+//			d-=System.currentTimeMillis();total+=d;
+//			Log.d("event","single day is "+d/1000.0+" sec");
+//
+//			Log.d("event","week total is "+total/1000.0+" sec");
+			
+			return e;
 		}
 
 		@Override
-		protected void onPostExecute(EventsAPI result) {
+		protected void onPostExecute(EventsReaderAPI result) {
 			setProgressBarIndeterminateVisibility(false);
 			Toast.makeText(ActivityDebugEvents.this, "done", Toast.LENGTH_SHORT).show();
 			//
--- UdemCalendrier/src/com/seboid/udemcalendrier/EventsAPI.java
+++ UdemCalendrier/src/com/seboid/udemcalendrier/EventsAPI.java
@@ -2,7 +2,9 @@ package com.seboid.udemcalendrier;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
@@ -31,42 +33,60 @@ import android.util.Log;
 // http://services.murmitoyen.com/udem/groupe/7/2013-01-26/2013-02-01/
 // http://services.murmitoyen.com/udem/souscategorie/64/2013-01-26/2013-02-01/
 //
-
+// on a aussi les series (tag d'un evenement en particulier)
+// http://services.murmitoyen.com/udem/serie/cfd4b81e5657376b6
+// (dans une serie, tous les evenements sont identiques, sauf pour les dates... ce sont de vrais
+// evenements qui doivent etre loader dans la bd. Ensuite on peut faire des select pour ramasser tout ca...
+//
+// http://services.murmitoyen.com/udem/<type>/<id>/<start>/<end>
+//
+// type=evenements, id=null, start/end obligatoire
+// type=categorie|groupe|souscategorie, id, start/end obligatoire
+// type=serie, id, start=null,end=null
+//
+// Load les data dans la bd.
+//
 public class EventsAPI {
 	// les informations intéressantes
 	//Drawable icone;
 
 	ArrayList<HashMap<String,String>> hmList;
-	
+
 	// null si pas d'erreur
 	String erreur;
 
-	EventsAPI() {
+	EventsAPI(String type,String id,String start,String end) {
 		erreur=null;
-		
-		String url="http://services.murmitoyen.com/udem/evenements/2013-03-26/2013-03-26";
+
+		//		String url="http://services.murmitoyen.com/udem/evenements/2013-03-26/2013-03-26";
+
+		String url="http://services.murmitoyen.com/udem/"+type;
+		if( !type.equals("evenements") ) url+="/"+id;
+		if( !type.equals("serie") ) url+="/"+start+"/"+end;
+
+//		Log.d("events","loading "+url);
 
 		hmList=new ArrayList<HashMap<String,String>>();
-		
+
 		try {
 			// lire la page web
 			HttpEntity page = getHttp(url);
 			String content = EntityUtils.toString(page,HTTP.UTF_8);
-			
+
 			// JSON format
 			JSONObject js = new JSONObject(content);
 			// extraire les informations courantes
 			JSONArray obs = js.getJSONArray("donnees");
-			
-			Log.d("event","nb child is "+obs.length());
+
+		//	Log.d("event","nb child is "+obs.length());
 
 			// un evenement
 			HashMap<String,String> hm;
-			
+
 			for(int i=0;i<obs.length();i++) {
 				JSONObject event=obs.getJSONObject(i);
-				Log.d("event",event.get("id")+" nbvalues= "+event.length());
-				
+			//	Log.d("event",event.get("id")+" nbvalues= "+event.length());
+
 				hm=new HashMap<String,String>();
 
 				// Html.fromHtml est supposemment lent... a verifier...
@@ -82,23 +102,26 @@ public class EventsAPI {
 				hm.put("vignette",event.get("vignette").toString());
 				hm.put("image",event.get("image").toString());
 				
+				long d1=date2epoch(hm.get("date"),null);
+			//	Log.d("events","date start = "+d1);
+				
 				hmList.add(hm);
 			}
-			
-//			temperature=obs.getString("temp_c")+"c";
-//			conditions=obs.getString("weather");
-//			ville = obs.getJSONObject("display_location").getString("full");
-//			
-//			long epoch= Long.parseLong(obs.getString("observation_epoch"));
-//			depuis=android.text.format.DateUtils.getRelativeTimeSpanString(epoch*1000);
-//			
-//			String iconName = obs.getString("icon");
-//			if( iconName!=null ) {
-//				Calendar cal= Calendar.getInstance();
-//				int h=cal.get(Calendar.HOUR_OF_DAY);
-//				// on devrait comparer a astronomy:sunset et sunrise
-//				icone=loadHttpImage("http://icons.wxug.com/i/c/a/"+((h>16 || h<7)?"nt_":"")+iconName+".gif");
-//			}
+
+			//			temperature=obs.getString("temp_c")+"c";
+			//			conditions=obs.getString("weather");
+			//			ville = obs.getJSONObject("display_location").getString("full");
+			//			
+			//			long epoch= Long.parseLong(obs.getString("observation_epoch"));
+			//			depuis=android.text.format.DateUtils.getRelativeTimeSpanString(epoch*1000);
+			//			
+			//			String iconName = obs.getString("icon");
+			//			if( iconName!=null ) {
+			//				Calendar cal= Calendar.getInstance();
+			//				int h=cal.get(Calendar.HOUR_OF_DAY);
+			//				// on devrait comparer a astronomy:sunset et sunrise
+			//				icone=loadHttpImage("http://icons.wxug.com/i/c/a/"+((h>16 || h<7)?"nt_":"")+iconName+".gif");
+			//			}
 		} catch (ClientProtocolException e) {
 			erreur="erreur http(protocol):"+e.getMessage();
 		} catch (IOException e) {
@@ -120,8 +143,8 @@ public class EventsAPI {
 		HttpResponse response = httpClient.execute(http);
 		return response.getEntity();    		
 	}
-		
-	
+
+
 	//
 	// lire une image avec un URL
 	//
@@ -131,5 +154,22 @@ public class EventsAPI {
 		return d;
 	}
 
+	private long date2epoch(String date,String time) {
+		String str = "Jun 13 2003 23:11:52.454 UTC";
+		SimpleDateFormat df;
+		Date d;
+		try {
+			if( time!=null ) {
+				df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				d=df.parse(date);
+			}else{
+				df=new SimpleDateFormat("yyyy-MM-dd");	    	
+				d=df.parse(date+" "+time);
+			}
+			return(d.getTime());
+		} catch( Exception e ) {
+			return(0);
+		}
+	}
 
 }
